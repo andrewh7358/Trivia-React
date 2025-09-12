b5d77f1d57b6982f0b66107abd41be4c8ebb8705
Andrew Huynh <andrew.h7358@gmail.com>
2025-09-12 15:14:42 -0700

    assert trivia is defined

--- src/api.ts
+++ src/api.ts
@@ -10,13 +10,13 @@ const url = 'https://opentdb.com/api.php?amount=5&type=multiple'
 
 export const getTrivia = async () => {
   if (trivia.length) {
-    return trivia.pop()
+    return trivia.pop()!
   }
   
   let res = await fetch(url)
   let data = await res.json() as { response_code: number, results: TriviaResponse[] }
 
-  while (data.response_code !== 0) {
+  while (data.response_code !== 0 && !data.results.length) {
     await new Promise(resolve => setTimeout(resolve, 2000))
     res = await fetch(url)
     data = await res.json() as { response_code: number, results: TriviaResponse[] }
@@ -24,5 +24,5 @@ export const getTrivia = async () => {
 
   trivia.push(...data.results)
 
-  return trivia.pop()
+  return trivia.pop()!
 }
